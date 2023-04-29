50649b4d80e8485d5659163a99da68d09372160a
bbpak <brianbpak@gmail.com>
2023-04-28 22:06:39 -0700

    fix footer z-index

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       left={0}
       right={0}
       color={useColorModeValue('gray.700', 'gray.200')}
-      z-index={-1}
+      zIndex={-1}
     >
       <Container
         color="white"
