7c298e7354e35a08ce377fcb6e38ee35714e2035
Bouhm <bbouhm@gmail.com>
2023-05-02 11:03:29 -0700

    even shorter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
       </Container>
       <Box>
         <Text pt={6} fontSize={'xs'} textAlign={'center'}>
-          Honkai: Star Rail and Genshin Impact are registered trademarks of miHoYo Co., Ltd. Hoyodle is not affiliated with miHoYo.
+          Honkai: Star Rail and Genshin Impact are registered trademarks of miHoYo Co., Ltd.
         </Text>
       </Box>
     </Box>
